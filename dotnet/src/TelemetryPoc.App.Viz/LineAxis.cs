c53cf8613e069ea628a1308cbcceacaa466b4020
Zabrodskiy Alexander <zabralex85@gmail.com>
2026-06-29 03:20:01 +0300

    feat(dotnet): App.Viz line-chart data + elapsed axis formatting (xUnit)

---
+++ dotnet/src/TelemetryPoc.App.Viz/LineAxis.cs
@@ -0,0 +1,12 @@
+namespace TelemetryPoc.App.Viz;
+
+public static class LineAxis
+{
+    public static string FormatElapsed(double sec)
+    {
+        var s = (long)Math.Max(0, Math.Floor(sec));
+        var m = s / 60;
+        var r = s % 60;
+        return m + ":" + r.ToString("00", System.Globalization.CultureInfo.InvariantCulture);
+    }
+}
---
+++ dotnet/src/TelemetryPoc.App.Viz/LineData.cs
@@ -0,0 +1,21 @@
+namespace TelemetryPoc.App.Viz;
+
+public static class LineData
+{
+    public static double[] ToSeconds(IReadOnlyList<long> xsMs)
+    {
+        var a = new double[xsMs.Count];
+        for (int i = 0; i < a.Length; i++) a[i] = xsMs[i] / 1000.0;
+        return a;
+    }
+
+    public static (double Min, double Max) ScrollWindow(IReadOnlyList<long> xsMs, long windowMs)
+    {
+        var w = Math.Max(1, windowMs);
+        if (xsMs.Count == 0) return (0, w / 1000.0);
+        var last = xsMs[^1];
+        var first = xsMs[0];
+        var min = Math.Max(first, last - w);
+        return (min / 1000.0, last / 1000.0);
+    }
+}
