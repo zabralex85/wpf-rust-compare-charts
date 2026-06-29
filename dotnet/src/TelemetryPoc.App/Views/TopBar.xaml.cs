e99983af74c4253ca78cf7eb8c5c2fa3a62dda54
Zabrodskiy Alexander <zabralex85@gmail.com>
2026-06-29 17:08:25 +0300

    feat(dotnet): frameless window + custom caption controls

--- dotnet/src/TelemetryPoc.App/Views/TopBar.xaml.cs
+++ dotnet/src/TelemetryPoc.App/Views/TopBar.xaml.cs
@@ -1,3 +1,4 @@
+using System.Windows;
 using System.Windows.Controls;
 
 namespace TelemetryPoc.App.Views;
@@ -5,4 +6,19 @@ namespace TelemetryPoc.App.Views;
 public partial class TopBar : UserControl
 {
     public TopBar() => InitializeComponent();
+
+    private void OnMinimize(object sender, RoutedEventArgs e)
+    {
+        var w = Window.GetWindow(this);
+        if (w is not null) w.WindowState = WindowState.Minimized;
+    }
+
+    private void OnMaximize(object sender, RoutedEventArgs e)
+    {
+        var w = Window.GetWindow(this);
+        if (w is not null)
+            w.WindowState = w.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+    }
+
+    private void OnClose(object sender, RoutedEventArgs e) => Window.GetWindow(this)?.Close();
 }
